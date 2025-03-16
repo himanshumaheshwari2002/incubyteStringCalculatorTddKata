736f71d0b8df9a5328220c16198ebab14af02396
himanshumaheshwari2001 <102040079+himanshumaheshwari2001@users.noreply.github.com>
2025-03-16 21:55:50 +0530

    empty string test case

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,6 +1,13 @@
-/*  FIRST SAMPLE FUNCTION*/
+/*  FIRST TEST CASE*/
 
+// our first test case should be an empty string to return a 0  which can directly check inside an if condition
+//eg - "" should return a 0
 
- function add(numbers) {
+/*  FIRST TEST CASE SOLUTION*/
 
- }
+function add(numbers) {
+    if (numbers === "") return 0;
+}
+
+let answer = add("")
+console.log(answer);
